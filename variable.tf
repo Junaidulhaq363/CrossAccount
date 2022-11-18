3e66e28d992c95336861f47fd7aa5e3a13f46d26
Junaidul Haq <haqjunaidul5@gmail.com>
2022-11-18 17:53:07 +0530

    first-commit

---
+++ variable.tf
@@ -0,0 +1,11 @@
+#AWS account region for dev account
+variable "region_dev" {
+  type    = string
+  default = "us-east-1"
+}
+
+# AWS account region for prod account
+variable "region_prod" {
+  type    = string
+  default = "us-east-1"
+}   
